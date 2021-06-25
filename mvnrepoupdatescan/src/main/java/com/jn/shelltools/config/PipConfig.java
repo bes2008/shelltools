60606f49149878c4de1e1de647988fe11da4242f
fjn <1194361820@qq.com>
2021-06-25 13:46:14 +0800

    * 完善配置

--- mvnrepoupdatescan/src/main/java/com/jn/shelltools/config/PipConfig.java
+++ mvnrepoupdatescan/src/main/java/com/jn/shelltools/config/PipConfig.java
@@ -1,11 +1,13 @@
 package com.jn.shelltools.config;
 
 import com.jn.agileway.feign.HttpConnectionContext;
-import com.jn.agileway.feign.HttpConnectionProperties;
 import com.jn.agileway.feign.RestServiceProvider;
 import com.jn.agileway.vfs.artifact.SynchronizedArtifactManager;
+import com.jn.agileway.vfs.artifact.repository.ArtifactRepositoryProperties;
 import com.jn.agileway.vfs.artifact.repository.DefaultArtifactRepositoryFactory;
 import com.jn.easyjson.core.factory.JsonFactorys;
+import com.jn.langx.util.collection.Collects;
+import com.jn.langx.util.function.Consumer;
 import com.jn.shelltools.core.pypi.PipPackageManager;
 import com.jn.shelltools.core.pypi.PipService;
 import org.apache.http.client.HttpClient;
@@ -14,41 +16,56 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class PipConfig {
 
     @Bean
-    @ConfigurationProperties(prefix = "pip.server")
-    public HttpConnectionProperties pipServerProperties(){
-        return new HttpConnectionProperties();
+    @ConfigurationProperties(prefix = "pip")
+    public PipPackageManagerProperties pipPackageManagerProperties() {
+        return new PipPackageManagerProperties();
     }
 
 
     @Bean
-    public SynchronizedArtifactManager pipArtifactManager(DefaultArtifactRepositoryFactory factory){
+    public SynchronizedArtifactManager pipArtifactManager(DefaultArtifactRepositoryFactory factory, PipPackageManagerProperties pipPackageManagerProperties) {
         SynchronizedArtifactManager artifactManager = new SynchronizedArtifactManager();
-        artifactManager.addSourceArtifactRepository();
+        List<ArtifactRepositoryProperties> repositories = pipPackageManagerProperties.getRepositories();
+        Collects.forEach(repositories, new Consumer<ArtifactRepositoryProperties>() {
+            @Override
+            public void accept(ArtifactRepositoryProperties artifactRepositoryProperties) {
+                factory.get(artifactRepositoryProperties);
+            }
+        });
+        Collects.forEach(pipPackageManagerProperties.getSources(), new Consumer<String>() {
+            @Override
+            public void accept(String source) {
+                artifactManager.addSource(factory.get(source));
+            }
+        });
+        artifactManager.setDestination(factory.get(pipPackageManagerProperties.getDestination()));
         return artifactManager;
     }
 
     @Bean("pipRestServiceProvider")
-    public RestServiceProvider pipRestServiceProvider(HttpConnectionProperties props, HttpClient httpClient) {
+    public RestServiceProvider pipRestServiceProvider(PipPackageManagerProperties props, HttpClient httpClient) {
         RestServiceProvider provider = new RestServiceProvider();
         provider.setJsonFactory(JsonFactorys.getJSONFactory());
         HttpConnectionContext context = new HttpConnectionContext();
-        context.setConfiguration(props);
+        context.setConfiguration(props.getServer());
         context.setHttpClient(httpClient);
         provider.setContext(context);
         return provider;
     }
 
     @Bean
-    public PipService pipService(@Qualifier("pipRestServiceProvider") RestServiceProvider restServiceProvider){
+    public PipService pipService(@Qualifier("pipRestServiceProvider") RestServiceProvider restServiceProvider) {
         return restServiceProvider.getService(PipService.class);
     }
 
     @Bean
-    public PipPackageManager pipPackageManager(PipService pipService){
+    public PipPackageManager pipPackageManager(PipService pipService) {
         PipPackageManager manager = new PipPackageManager();
         manager.setService(pipService);
         return manager;
---
+++ mvnrepoupdatescan/src/main/java/com/jn/shelltools/config/PipPackageManagerProperties.java
@@ -0,0 +1,36 @@
+package com.jn.shelltools.config;
+
+import com.jn.agileway.feign.HttpConnectionProperties;
+import com.jn.agileway.vfs.artifact.ArtifactProperties;
+
+import java.util.List;
+
+public class PipPackageManagerProperties extends ArtifactProperties {
+    private HttpConnectionProperties server;
+    private String destination;
+    private List<String> sources;
+
+    public HttpConnectionProperties getServer() {
+        return server;
+    }
+
+    public void setServer(HttpConnectionProperties server) {
+        this.server = server;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public List<String> getSources() {
+        return sources;
+    }
+
+    public void setSources(List<String> sources) {
+        this.sources = sources;
+    }
+}

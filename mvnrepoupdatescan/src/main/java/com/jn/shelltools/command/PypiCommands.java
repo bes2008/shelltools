f91c4f77fdae5ec8f606cac6a87d3c93c8f97f9b
fjn <fjn@163.com>
2021-11-22 17:58:02 +0800

    * 增强 pypi 下载命令，支持批量下载

--- mvnrepoupdatescan/src/main/java/com/jn/shelltools/command/PypiCommands.java
+++ mvnrepoupdatescan/src/main/java/com/jn/shelltools/command/PypiCommands.java
@@ -2,6 +2,14 @@ package com.jn.shelltools.command;
 
 import com.jn.easyjson.core.JSON;
 import com.jn.easyjson.core.JSONBuilderProvider;
+import com.jn.langx.io.resource.Resource;
+import com.jn.langx.io.resource.Resources;
+import com.jn.langx.util.Strings;
+import com.jn.langx.util.SystemPropertys;
+import com.jn.langx.util.collection.Collects;
+import com.jn.langx.util.function.Consumer;
+import com.jn.langx.util.io.Charsets;
+import com.jn.langx.util.io.IOs;
 import com.jn.shelltools.core.pypi.PypiPackageManager;
 import com.jn.shelltools.core.pypi.packagemetadata.PipPackageMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +17,11 @@ import org.springframework.shell.standard.ShellCommandGroup;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
+
+import java.io.File;
+import java.nio.channels.Channels;
+import java.nio.charset.Charset;
+import java.util.List;
 
 @ShellComponent
 @ShellCommandGroup("pip")
@@ -28,9 +41,32 @@ public class PypiCommands {
 
     @ShellMethod(key = "pip download", value = "download a package (and its dependencies) to the out directory")
     public void downloadPackage(
-            @ShellOption(value = "--package", help = "the package name") String packageName,
+            @ShellOption(value = "--package", help = "the package name or the requirements file path") String packageName,
             @ShellOption(value = "--with-deps", defaultValue = "true", help = "with its dependencies ?") boolean withDeps,
             @ShellOption(value = "--out", defaultValue = "__NULL__", help = "the out directory") String outDirectory) {
-        pypiPackageManager.downloadPackage(packageName, withDeps, outDirectory);
+
+        if (Strings.isBlank(outDirectory)) {
+            outDirectory = SystemPropertys.getUserWorkDir();
+        }
+        final String out = outDirectory;
+        File file = new File(packageName);
+        if (!file.exists()) {
+            pypiPackageManager.downloadPackage(packageName, withDeps, out);
+        } else {
+            Resource resource = Resources.loadFileResource(file);
+            List<String> lines = Collects.emptyArrayList();
+            Resources.readUsingDelimiter(resource, "\n", Charsets.UTF_8, new Consumer<String>() {
+                @Override
+                public void accept(String line) {
+                    lines.add(line);
+                }
+            });
+            Collects.forEach(lines, new Consumer<String>() {
+                @Override
+                public void accept(String line) {
+                    pypiPackageManager.downloadPackage(line, withDeps, out);
+                }
+            });
+        }
     }
 }

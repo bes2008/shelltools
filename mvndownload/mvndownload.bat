075f2e0560eb54fd0ee9a22c859adb5e05606695
fjn1194361820 <fs1194361820@163.com>
2023-06-01 15:51:25 +0800

    * 更新依赖版本

--- mvndownload/mvndownload.bat
+++ mvndownload/mvndownload.bat
@@ -1,6 +1,6 @@
 call setEnv.bat
-call mvndownloadsource.bat
-REM start "sources downloading" cmd /k call mvndownloadsource.bat
-start "javadoc downloading" cmd /k call mvndownloaddocs.bat
+REM call mvndownloadsource.bat
+start "sources downloading" cmd /k call mvndownloadsource.bat
+REM start "javadoc downloading" cmd /k call mvndownloaddocs.bat
 mvn clean package --settings %MAVEN_REPO_DOWNLOAD%
 pause

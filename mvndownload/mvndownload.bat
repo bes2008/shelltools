8f11e20bbef0c997712a235030ced86ff2bbbfe5
fjn1194361820 <fs1194361820@163.com>
2024-08-09 15:37:23 +0800

    * 支持更多的类库

--- mvndownload/mvndownload.bat
+++ mvndownload/mvndownload.bat
@@ -3,5 +3,9 @@ REM call mvndownloadsource.bat
 start "sources downloading" cmd /k call mvndownloadsource.bat
 REM start "javadoc downloading" cmd /k call mvndownloaddocs.bat
 REM mvn clean package --settings %MAVEN_REPO_DOWNLOAD% -T 3
-mvn clean package --settings %MAVEN_REPO_DOWNLOAD% -T 3
+mvn clean package --settings %MAVEN_REPO_DOWNLOAD% -T 1
+
+REM start "sources downloading" cmd /k call mvndownloadsource.bat
+
+timeouts /t 60000 > nul
 pause
